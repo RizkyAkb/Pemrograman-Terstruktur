d8b5e37c68c65549e917286d4d76c7e15d0d1463
Rizky Akbar Wahyu Putranto <89757057+RizkyAkb@users.noreply.github.com>
2021-10-10 18:55:01 +0700

    Initial Commit

--- Chapter4/Latihan4.py
+++ Chapter4/Latihan4.py
@@ -1,10 +1,29 @@
-jarak_ab = 125
-kec_ab = 62
-waktu_ab = 125 / 62 * 60
-print (waktu_ab)
-jarak_bc = 256
-kec_bc = 70
-waktu_bc = 256/70
-print (waktu_bc)
-istirahat = 45
+jrk_AB = 125
+jrk_BC = 256
+kec_AB = 62
+kec_BC = 70
 
+jambrkt = 6
+mntbrkt = 00
+
+mntistirahat = 45 / 60
+
+waktu_AB = round(jrk_AB / kec_AB, 2)
+waktu_BC = round(jrk_BC / kec_BC, 2)
+
+totalwaktu = waktu_AB + waktu_BC + mntistirahat
+totalwaktu = round(totalwaktu, 2)
+
+totaljam = totalwaktu // 1
+jamsampai = totaljam + jambrkt
+
+totalmnt = totalwaktu % 1
+totalmnt = round(totalmnt * 60)
+mntsampai = round(totalmnt + mntbrkt)
+
+print('Waktu yang diperlukan dari kota A ke B adalah', waktu_AB, 'jam')
+print('Waktu yang diperlukan dari kota B ke C adalah', waktu_BC, 'jam')
+print('Total waktu perjalanan + istirahat 45 menit adalah', totalwaktu, 'jam')
+print('Atau', round(totaljam), "jam", totalmnt, 'menit')
+print('Jika pak Amir berangkat pada jam', jambrkt, 'lebih', mntbrkt, 'menit')
+print('Maka pak Amir akan sampai pada jam', round(jamsampai), 'lebih', mntsampai, 'menit')
