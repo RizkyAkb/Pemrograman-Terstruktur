2b6190f7c4b57eda90db5ff52fce7f59f1c1931d
Rizky Akbar Wahyu Putranto <89757057+RizkyAkb@users.noreply.github.com>
2021-10-10 19:49:23 +0700

    Update Latihan4.py

--- Chapter4/Latihan4.py
+++ Chapter4/Latihan4.py
@@ -18,8 +18,8 @@ totaljam = totalwaktu // 1
 jamsampai = totaljam + jambrkt
 
 totalmnt = totalwaktu % 1
-totalmnt = round(totalmnt * 60)
-mntsampai = round(totalmnt + mntbrkt)
+totalmnt = round(totalmnt * 60, 1)
+mntsampai = round(totalmnt + mntbrkt, 1)
 
 print('Waktu yang diperlukan dari kota A ke B adalah', waktu_AB, 'jam')
 print('Waktu yang diperlukan dari kota B ke C adalah', waktu_BC, 'jam')
@@ -27,4 +27,3 @@ print('Total waktu perjalanan + istirahat 45 menit adalah', totalwaktu, 'jam')
 print('Atau', round(totaljam), "jam", totalmnt, 'menit')
 print('Jika pak Amir berangkat pada jam', jambrkt, 'lebih', mntbrkt, 'menit')
 print('Maka pak Amir akan sampai pada jam', round(jamsampai), 'lebih', mntsampai, 'menit')
-
