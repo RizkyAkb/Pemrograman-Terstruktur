1aa88494fa2daed4f724c36d001836f52cfa2f46
RizkyAkb <rawp58@gmail.com>
2021-10-16 22:31:53 +0700

    Chapter5

--- Chapter5/Latihan1/Latihan1.py
+++ Chapter5/Latihan1/Latihan1.py
@@ -2,7 +2,7 @@ bhsind = int(input('Nilai B.Indonesia: '))
 ipa = int(input   ('Nilai IPA        : '))
 mtk = int(input   ('Nilai Matematika : '))
 
-if (bhsind > 60) and (ipa > 60) and (mtk > 70):
+if (bhsind >= 60) and (ipa >= 60) and (mtk > 70):
     print ('Status kelulusan : Selamat anda LULUS')
 else:
     print ('Status kelulusan : TIDAK LULUS')
