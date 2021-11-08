40315f231c3b901eddb738cf5c8195351f2c9433
Rizky Akbar Wahyu Putranto <89757057+RizkyAkb@users.noreply.github.com>
2021-11-08 18:42:37 +0700

    Delete Chapter8 directory

--- Chapter8/latihan3.py
+++
@@ -1,9 +0,0 @@
-data = []
-
-mhs = int(input('Berapa banyak mahasiswa yang ingin di inputkan?: '))
-for i in range(mhs):
-    nama = input('Masukkan nama: ')
-    data.append(nama)
- 
-for nama in data:
-    print(nama, '('+ str(len(nama)) +'karakter)')
